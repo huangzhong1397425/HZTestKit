6c7c3bc59b6d92c304485245b390ac102bacba95
baidu <baidu@MacBook-Pro.local>
2016-06-06 14:04:52 +0800

    add hztestkit

---
+++ HZTestKit.h
@@ -0,0 +1,15 @@
+//
+//  HZTestKit.h
+//  Test
+//
+//  Created by baidu on 16/6/6.
+//  Copyright © 2016年 baidu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HZTestKit : NSObject
+
+- (void)sayHello;
+
+@end
---
+++ HZTestKit.m
@@ -0,0 +1,16 @@
+//
+//  HZTestKit.m
+//  Test
+//
+//  Created by baidu on 16/6/6.
+//  Copyright © 2016年 baidu. All rights reserved.
+//
+
+#import "HZTestKit.h"
+
+@implementation HZTestKit
+
+- (void)sayHello{
+    NSLog(@"hello!");
+}
+@end
